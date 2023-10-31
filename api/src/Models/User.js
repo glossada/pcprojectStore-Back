970b2ba2219d5b76d828c4a33d3a8a13a2059dbf
glossada <gabriellossada@gmail.com>
2023-10-31 19:40:53 -0300

    server runing and some models

---
+++ api/src/Models/User.js
@@ -0,0 +1,58 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+    sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4, 
+            unique: true,
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notEmpty: true,
+            },
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            },
+          },
+          address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notEmpty: true,
+            },
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notEmpty: true,
+            },
+          },
+          phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              notEmpty: true,
+            },
+          },
+          role: {
+            type: DataTypes.ENUM("user", "admin", "BAN"),
+            defaultValue: "user",
+          },
+          provider: {
+            type: DataTypes.ENUM("local", "google", "facebook"),
+            defaultValue: "local",
+          },
+  
+    },{ timestamps: false });
+  };
